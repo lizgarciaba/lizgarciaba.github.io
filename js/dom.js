2fd433cbf7f4dca1d38203bddaa95009f10b9cb4
lizgarciaba <lizgabarre@gmail.com>
2021-10-05 17:06:11 -0500

    Sitios Webs

---
+++ js/dom.js
@@ -0,0 +1,7 @@
+function testDom()
+{
+	var obj = document.getElementById("parr");
+
+	obj.innerHTML = "<spam style='color:red; font-size:0.8em;'>Ing. Elizabeth García B<br>lizgarciaba@gmail.com <br>Celular:0984969143<br>Guayaquil-Ecuador</spam>"; 
+
+}
